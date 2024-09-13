726e2e0bb3e254c7dbbd22d71a9862aaf9cd123b
PRITHIV SAKTHI U R <117709109+PRITHIVSAKTHIUR@users.noreply.github.com>
2024-09-13 22:00:50 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,5 @@
 ﻿INSERT INTO Clients (FirstName, LastName, Email, Phone, Address, Status, CreateAt)
 VALUES
-('Prithiv','Sakthi','Prithiv.Sakthi@gmail.com','9944193417','North st., GOBI','New',CURRENT_TIMESTAMP),
+('Prithiv','Sakthi','Pawdadra.Sakthi@gmail.com','99441454417','North st., GOBI','New',CURRENT_TIMESTAMP),
 ('Navamani','Balu','Navan.Balu@gmail.com','8978677233','Pillayar st., SATHY','Inactive',CURRENT_TIMESTAMP),
 ('Giri','Murugan','giri.m@gmail.com','8488588589','Near BS., SANKARI','New',CURRENT_TIMESTAMP);
